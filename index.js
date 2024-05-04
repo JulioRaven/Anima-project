5d2d211b3d0aa8e816ee60cc6ca053c4bdeeb97a
JulioRaven <114471308+JulioRaven@users.noreply.github.com>
2024-05-05 00:12:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -91,6 +91,8 @@ app.get('/generate-image', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
